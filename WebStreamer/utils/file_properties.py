83d0e6e13bd528b2ff38514983183642b089bf24
Mohanish <111619160+Mohanish7777777@users.noreply.github.com>
2023-12-03 12:22:24 +0530

    Update file_properties.py

--- WebStreamer/utils/file_properties.py
+++ WebStreamer/utils/file_properties.py
@@ -83,12 +83,12 @@ async def gen_link(m: Message,log_msg: Messages, from_channel: bool):
     page_link = f"{Var.URL}watch/{get_hash(log_msg)}{log_msg.id}"
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     Stream_Text=lang.stream_msg_text.format(file_name, file_size, stream_link, page_link)
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(url=page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
 
     if from_channel:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(url=page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
     else:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)],
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Stream", web_app=WebAppInfo(url=page_link)), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)],
             [InlineKeyboardButton("❌ Delete Link", callback_data=f"msgdelconf2_{log_msg.id}_{get_media_file_unique_id(log_msg)}")]])
 
     return reply_markup, Stream_Text, stream_link
